950223160953b9fbcd9796dac6b116e7c4a796bd
YEMENI? <altmymwyd80@gmail.com>
2026-06-05 09:20:53 +0300

    Update Create exe.bat

--- Build/Create exe.bat
+++ Build/Create exe.bat
@@ -1,3 +1,3 @@
 @echo off
-python -m PyInstaller --noconfirm --windowed --onefile --uac-admin --distpath dist --icon Rme.ico --collect-all customtkinter --collect-all psutil re4_master_editor.py
+python -m PyInstaller --noconfirm --windowed --onefile --noupx --uac-admin --distpath . --icon Rme.ico --collect-all customtkinter --collect-all psutil re4_master_editor.py
 pause
